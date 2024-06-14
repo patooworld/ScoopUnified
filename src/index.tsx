cebb57ceb482e3dfb63ded3211d59aa90eb4ad73
PATOOWORLD <officialpatoo@icloud.com>
2024-06-14 09:03:15 -0700

    Update index.tsx
    migrating platforms tools with scoop

--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,32 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+  "Version": 1,
+  "WorkspaceRootPath": "C:\\Users\\SAMUELCHIBUNNA\\",
+  "Documents": [],
+  "DocumentGroupContainers": [
+    {
+      "Orientation": 0,
+      "VerticalTabListWidth": 256,
+      "DocumentGroups": [
+        {
+          "DockedWidth": 200,
+          "SelectedChildIndex": -1,
+          "Children": [
+            {
+              "$type": "Bookmark",
+              "Name": "ST:128:0:{75188d03-9892-4ae2-abf1-207126247ce5}"
+            },
+            {
+              "$type": "Bookmark",
+              "Name": "ST:0:0:{1c64b9c2-e352-428e-a56d-0ace190b99a6}"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
